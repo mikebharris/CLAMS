bb5cf24ee66ca423a39fab65728c1b98ef475dda
Mike Harris <m.harris@elsevier.com>
2022-06-07 17:16:03 +0100

    Reformatted

--- frontend/src/Attendees.svelte
+++ frontend/src/Attendees.svelte
@@ -1,33 +1,37 @@
 <script lang="ts">
-	import { beforeUpdate } from "svelte";
-	import Summary from "./Summary.svelte";
+    import {beforeUpdate} from "svelte";
+    import Summary from "./Summary.svelte";
 
-	let attendees: JSON[];
+    let attendees: JSON[];
 
-	$: fetch(`${process.env.API_GATEWAY_URL}/attendees`)
-		.then(r => r.json())
-		.then(data => {
-			attendees = data.Attendees;
-			window.scrollTo(0, 0);
-		});
+    $: fetch(`${process.env.API_GATEWAY_URL}/attendees`)
+        .then(r => r.json())
+        .then(data => {
+            attendees = data.Attendees;
+            window.scrollTo(0, 0);
+        });
 </script>
 
 {#if attendees}
-	{#each attendees as attendee, i}
-		<Summary {attendee} {i}/>
-	{/each}
+    {#each attendees as attendee, i}
+        <Summary {attendee} {i}/>
+    {/each}
 {:else}
-	<p class="loading">loading...</p>
+    <p class="loading">loading...</p>
 {/if}
 
 <style>
-	.loading {
-		opacity: 0;
-		animation: 0.4s 0.8s forwards fade-in;
-	}
+    .loading {
+        opacity: 0;
+        animation: 0.4s 0.8s forwards fade-in;
+    }
 
-	@keyframes fade-in {
-		from { opacity: 0; }
-		to { opacity: 1; }
-	}
+    @keyframes fade-in {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
 </style>
