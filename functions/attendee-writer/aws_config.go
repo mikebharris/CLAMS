f625d3de6d7ef8e3fe5703e1761e6ac92b96a17e
Mike Harris <m.harris@elsevier.com>
2022-10-09 14:20:05 +0100

    Improve test coverage for AWS Config in Writer

--- functions/attendee-writer/aws_config.go
+++ functions/attendee-writer/aws_config.go
@@ -9,22 +9,25 @@ import (
 	"os"
 )
 
+var loadDefaultConfig = config.LoadDefaultConfig
+
 func newAwsConfig(awsRegion string) (*aws.Config, error) {
 	dynamoEndpointOverride := os.Getenv("DYNAMO_ENDPOINT_OVERRIDE")
 
-	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(awsRegion))
+	cfg, err := loadDefaultConfig(context.Background(), config.WithRegion(awsRegion))
 	if err != nil {
 		return nil, fmt.Errorf("loading config: %v", err)
 	}
 
-	if len(dynamoEndpointOverride) > 0 {
-		defaultEndpointResolver := cfg.EndpointResolver
-		cfg.EndpointResolver = aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
-			if service == dynamodb.ServiceID && len(dynamoEndpointOverride) > 0 {
-				return aws.Endpoint{URL: dynamoEndpointOverride}, nil
-			}
-			return defaultEndpointResolver.ResolveEndpoint(service, region)
-		})
+	if len(dynamoEndpointOverride) == 0 {
+		return &cfg, nil
 	}
+
+	cfg.EndpointResolverWithOptions = aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+		if service == dynamodb.ServiceID {
+			return aws.Endpoint{URL: dynamoEndpointOverride}, nil
+		}
+		return cfg.EndpointResolver.ResolveEndpoint(service, region)
+	})
 	return &cfg, nil
 }
--- functions/attendee-writer/aws_config_test.go
+++ functions/attendee-writer/aws_config_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"os"
 	"testing"
 
@@ -29,4 +33,19 @@ func TestShouldReturnDefaultConfigurationWhenDynamoDbEndpointIsNotOverridden(t *
 	// Then
 	assert.Nil(t, err)
 	assert.NotNil(t, configWithEndpointOverride)
+}
+
+func TestShouldReturnErrorWhenLoadingConfigurationReturnsError(t *testing.T) {
+	// Given
+	loadDefaultConfig = func(ctx context.Context, optFns ...func(*config.LoadOptions) error) (cfg aws.Config, err error) {
+		return aws.Config{}, fmt.Errorf("some error")
+	}
+
+	// When
+	configWithEndpointOverride, err := newAwsConfig("us-east-1")
+
+	// Then
+	assert.NotNil(t, err)
+	assert.Equal(t, fmt.Errorf("loading config: some error"), err)
+	assert.Nil(t, configWithEndpointOverride)
 }
